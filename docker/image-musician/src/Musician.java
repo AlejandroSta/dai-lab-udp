1c0adb3223c750affb20d7fecbc6cfe43487513f
AlejandroSta <alejandro.stadlin@heig-vd.ch>
2024-01-23 11:18:06 +0100

    tentative musician

---
+++ docker/image-musician/src/Musician.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.net.MulticastSocket;
+import java.net.InetSocketAddress;
+import java.net.NetworkInterface;
+import java.net.DatagramPacket;
+import static java.nio.charset.StandardCharsets.*;
+
+
+public class Auditor {
+    final static String IPADDRESS = "239.255.22.5";
+    final static int PORT = 9904;
+
+    public static void main(String[] args) {
+        try (DatagramSocket socket = new DatagramSocket()) {
+
+            String message = "Hello group members!";
+            byte[] payload = message.getBytes(UTF_8);
+            InetSocketAddress dest_address = new InetSocketAddress(IPADDRESS, 44444);
+            var packet = new DatagramPacket(payload, payload.length, dest_address);
+            socket.send(packet);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
